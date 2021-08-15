6213cf1634f73e3c08de8398a111a840d0716f11
Markussim <markus.simonsen@gmail.com>
2021-08-15 13:27:39 +0200

    Moved qr generator code

--- index.php
+++ index.php
@@ -95,8 +95,7 @@
                                             console.log("this.responseText");
 
                                             document.getElementById("text").innerHTML = "";
-                                            document.getElementById("done").innerHTML = "<div id=\"qr\"></div>";
-                                            document.getElementById("done").innerHTML = "<button onclick=\"copyText()\">Kopiera länk</button><button onclick=\"location.reload(true);\">Ladda upp en till fil</button>";
+                                            document.getElementById("done").innerHTML = "<div id=\"qr\"></div><button onclick=\"copyText()\">Kopiera länk</button><button onclick=\"location.reload(true);\">Ladda upp en till fil</button>";
 
                                             document.getElementById("myInput").value = "https://uploads.marksism.space" + this.responseText
 
